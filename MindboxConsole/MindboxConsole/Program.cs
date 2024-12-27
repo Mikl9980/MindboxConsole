34b7a3bec98278b656a763a5764b204d0afd69c0
Mikl9980 <52324929+Mikl9980@users.noreply.github.com>
2024-12-27 17:31:51 +0700

    Add files via upload

---
+++ MindboxConsole/MindboxConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Mindbox;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Пример использования библиотеки
+
+        // Создаем круг с радиусом 5
+        var circle = new Circle(5);
+        Console.WriteLine($"Площадь круга: {circle.CalculateArea()}");
+
+        // Создаем треугольник со сторонами 3, 4, 5
+        var triangle = new Triangle(3, 4, 5);
+        Console.WriteLine($"Площадь треугольника: {triangle.CalculateArea()}");
+        Console.WriteLine($"Треугольник прямоугольный: {triangle.IsRightAngled()}");
+
+        // Пример с неправильным треугольником (вызовет исключение)
+        try
+        {
+            var invalidTriangle = new Triangle(1, 2, 3);
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+}
---
+++ MindboxConsole/MindboxConsole/ShapeTests.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+using Mindbox;
+
+public class ShapeTests
+{
+    [Fact]
+    public void TestCircleArea()
+    {
+        var circle = new Circle(5);
+        var area = circle.CalculateArea();
+        Assert.Equal(78.54, area, 2); // Проверяем с точностью до 2 знаков
+    }
+
+    [Fact]
+    public void TestTriangleArea()
+    {
+        var triangle = new Triangle(3, 4, 5);
+        var area = triangle.CalculateArea();
+        Assert.Equal(6, area);
+    }
+
+    [Fact]
+    public void TestRightTriangle()
+    {
+        var triangle = new Triangle(3, 4, 5);
+        var isRight = triangle.IsRightAngled();
+        Assert.True(isRight);
+    }
+
+    [Fact]
+    public void TestInvalidTriangle()
+    {
+        Assert.Throws<ArgumentException>(() => new Triangle(1, 2, 3));
+    }
+}
